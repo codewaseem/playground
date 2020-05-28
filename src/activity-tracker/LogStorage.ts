01791abc1342464dbba7f53115ec1b871d15b1b3
codewaseem <codewaseem@gmail.com>
2020-05-28 23:02:35 +0530

    update demo

--- src/activity-tracker/LogStorage.ts
+++ src/activity-tracker/LogStorage.ts
@@ -2,7 +2,6 @@ import { TrackerLogStorage, ActivityLog } from ".";
 
 export default class LogStorage implements TrackerLogStorage {
     save(data: ActivityLog): Promise<ActivityLog> {
-        console.log(data);
         return Promise.resolve(data);
     }
 }
--- src/activity-tracker/demo.ts
+++ src/activity-tracker/demo.ts
@@ -3,6 +3,10 @@ import LogStorage from "./LogStorage";
 
 (async () => {
     const tracker = new Tracker(new LogStorage());
-
     tracker.start();
+
+    setInterval(() => {
+        console.log(tracker.getLogData());
+    }, Tracker.DEFAULT_TIME_INTERVAL);
+
 })();
