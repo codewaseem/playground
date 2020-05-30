fd38f77137830acf442bc8907edf118445b99bed
codewaseem <codewaseem@gmail.com>
2020-05-30 21:06:49 +0530

    test iohook

---
+++ test-io.ts
@@ -0,0 +1,54 @@
+import iohook from "iohook";
+
+class IoHookManager {
+
+    private _mouseclicks = 0
+    private _keystrokes = 0;
+
+    private subscribers: Function[] = [];
+
+    start() {
+        iohook.start();
+
+        iohook.on("keyup", this._incrementKeystrokes);
+
+        iohook.on("mouseup", this._incrementMouseClicks);
+
+        iohook.on("mousewheel", this._incrementMouseClicks);
+
+    }
+
+    private _incrementMouseClicks = () => {
+        this._mouseclicks++;
+        this._onChange();
+    }
+
+    private _incrementKeystrokes = () => {
+        this._keystrokes++;
+        this._onChange();
+    }
+
+    getData() {
+        return {
+            mouseclicks: this._mouseclicks,
+            keystrokes: this._keystrokes
+        }
+    }
+
+    subscribe(fn: (data: {
+        mouseclicks: number,
+        keystrokes: number
+    }) => void) {
+        this.subscribers.push(fn);
+    }
+
+    private _onChange() {
+        this.subscribers.forEach(fn => fn(this.getData()))
+    }
+}
+
+const ioHookManager = new IoHookManager();
+
+ioHookManager.start();
+
+ioHookManager.subscribe(console.log)
