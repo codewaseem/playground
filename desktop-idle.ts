ddc5791405788052312f3d97586a49045e3f6db2
codewaseem <codewaseem@gmail.com>
2020-05-31 15:58:07 +0530

    make it little better.

--- desktop-idle.ts
+++ desktop-idle.ts
@@ -3,21 +3,23 @@ import desktopIdle from "desktop-idle";
 
 class IdleTimeTracker {
 
-    private _prevIdleTime = 0;
-    private _currentIdleTime = 0;
+    private _prevIdleTime = desktopIdle.getIdleTime();
+    private _currentIdleTime = this._prevIdleTime;
+    private _newTotalIdleTime = 0;
     private _totalIdleTime = 0;
 
     getTotalIdleTime() {
         this._currentIdleTime = desktopIdle.getIdleTime();
+        this._totalIdleTime = this._newTotalIdleTime + this._prevIdleTime;
 
         if (this._isNewIdleState()) {
-            this._totalIdleTime += this._prevIdleTime;
+            this._newTotalIdleTime += this._prevIdleTime;
             this._prevIdleTime = 0;
         } else {
             this._prevIdleTime = this._currentIdleTime;
         }
 
-        return this._totalIdleTime + this._prevIdleTime;
+        return this._totalIdleTime;
     }
 
     private _isNewIdleState() {
