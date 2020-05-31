10dfd10505b034d6e8fcfbe109f3a6116ff36f79
codewaseem <codewaseem@gmail.com>
2020-05-31 15:46:58 +0530

    simplify

--- desktop-idle.ts
+++ desktop-idle.ts
@@ -7,28 +7,17 @@ class IdleTimeTracker {
     private _currentIdleTime = 0;
     private _totalIdleTime = 0;
 
-    constructor() {
+    getTotalIdleTime() {
         this._currentIdleTime = desktopIdle.getIdleTime();
 
-        const startTimer = () => setTimeout(() => {
-            this._currentIdleTime = desktopIdle.getIdleTime();
+        if (this._isNewIdleState()) {
+            this._totalIdleTime += this._prevIdleTime;
+            this._prevIdleTime = 0;
+        } else {
+            this._prevIdleTime = this._currentIdleTime;
+        }
 
-            if (this._isNewIdleState()) {
-                this._totalIdleTime += this._prevIdleTime;
-                this._prevIdleTime = 0;
-            } else {
-                this._prevIdleTime = this._currentIdleTime;
-            }
-
-            startTimer();
-
-        }, 10000);
-
-        startTimer();
-    }
-
-    getTotalIdleTime() {
-        return this._totalIdleTime + this._currentIdleTime;
+        return this._totalIdleTime + this._prevIdleTime;
     }
 
     private _isNewIdleState() {
